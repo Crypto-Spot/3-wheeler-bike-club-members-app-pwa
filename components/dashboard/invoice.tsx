387950b9e7fdf6c65a82f838aed4f857eb3e7610
Tickether <tickether@gmail.com>
2024-10-21 01:31:27 +0000

    fix: direct attest

--- components/dashboard/invoice.tsx
+++ components/dashboard/invoice.tsx
@@ -1,14 +1,15 @@
 import { OffchainAttestation } from "@/hooks/transactions/useGetAttestations";
 import { Card } from "../ui/card";
-import { unpaidUID, USDC } from "@/utils/constants/addresses";
+import { unpaidUID } from "@/utils/constants/addresses";
 import { useState } from "react";
 import { Ramp } from "./ramp";
 import { Button } from "../ui/button";
-import { useWatchContractEvent } from "wagmi";
-import { base } from "viem/chains";
-import { erc20Abi } from "viem";
+//mport { useWatchContractEvent } from "wagmi";
+//import { base } from "viem/chains";
+//import { erc20Abi } from "viem";
 import { sendInvoiceAttestationOnchain } from "@/utils/attest/sendAttestation";
 import { updateAttestationAction } from "@/app/actions/attestation/updateAttestationAction";
+
 //import { getInvoiceAttestationOnchain } from "@/utils/attest/getAttestation";
 //import { Attestation } from "@ethereum-attestation-service/eas-sdk";
 
@@ -19,9 +20,9 @@ export function Invoice ({ attestation }: InvoiceProps) {
 
     const [openCashRamp, setOpenCashRamp] = useState<boolean>(false)
     //const [paying, setPaying] = useState<boolean>(false)
-    const [reference, setReference] = useState<string | null>(null)
+    //const [reference, setReference] = useState<string | null>(null)
     //const [onchainAttest, setOnchainAttest] = useState<Attestation | null>(null)
-
+/*
     const doCashRampPay = () => {
         
         setOpenCashRamp(true)
@@ -29,6 +30,7 @@ export function Invoice ({ attestation }: InvoiceProps) {
         setReference(ref)
 
     }
+*/
     async function dooAll(value: bigint | undefined) {
         // make onchain attestation
         const timestampNow = Date.now();
@@ -43,23 +45,22 @@ export function Invoice ({ attestation }: InvoiceProps) {
         // send ussdc to treasury wallet/splitter(90/10) 
             
     }
+    /*
     useWatchContractEvent({
         address: USDC,
-        chainId: base.id,
+        chainId: 8453,
         abi: erc20Abi,
-        eventName: 'Transfer',
+        eventName: "Transfer",
         args: {
             to: attestation.address as `0x${string}`
             
         },
         onLogs(logs)  {
             console.log('New logs!', logs[0].args.value)
-            //if (paying) {
-               
-                dooAll(logs[0].args.value)
-            //}
+            dooAll(logs[0].args.value)
         },
     })
+    */
 
     return(
         <>
@@ -84,14 +85,15 @@ export function Invoice ({ attestation }: InvoiceProps) {
                     onClick={()=>{
                         
                         //setPaying(true)
-                        doCashRampPay()
+                        //doCashRampPay()
+                        dooAll(BigInt(1000))
                         
                     }}
                 >
                     Pay
                 </Button>
             </Card>
-            {openCashRamp && <Ramp setOpenRamp={setOpenCashRamp} paymentType="deposit" address={attestation?.address as `0x${string}`} reference={reference!} />}
+            {openCashRamp && <Ramp setOpenRamp={setOpenCashRamp} paymentType="deposit" address={attestation?.address as `0x${string}`} reference={"reference"} />}
         </>
     )
 }
--- utils/config.ts
+++ utils/config.ts
@@ -1,16 +1,15 @@
 import { createConfig, http, cookieStorage, createStorage } from "wagmi"
-import { base, baseSepolia } from "wagmi/chains"
+import { base } from "wagmi/chains"
 
 //export const chains=
 export const config = createConfig({
-  chains: [ base, baseSepolia ],
+  chains: [ base ],
   ssr: true,
   storage: createStorage({  
     storage: cookieStorage, 
   }),
   transports: {
     [base.id]: http(),
-    [baseSepolia.id]: http()
   },
 })
 
